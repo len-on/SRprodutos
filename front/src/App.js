79539f32dac25d96ca35ef2a82f305f7cb0f734e
len-on <lenon.assalin@gmail.com>
2023-04-12 17:53:47 -0300

    população da tabela produtos no banco, adição do cors para conversão em json entre o spring e o react

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
+  //useState
   const [btnCadastrar, setBtnCadastrar] = useState(false);
+  const [produtos, setProdutos] = useState([]);
 
+  //UseEffect
+  useEffect(() =>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  //Retorno
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela />
     </div>
